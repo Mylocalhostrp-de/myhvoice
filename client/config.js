6b09d28411f505dc7ea822b87aa1f8842eddbc4d
Alex <92964309+alex-Studioone@users.noreply.github.com>
2025-08-10 06:15:36 +0200

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,7 @@
+export default {
+    KEY_VOICE_RANGE: 90,    // Z (Voice-Range wechseln)
+    KEY_FUNKTASTE: 186,     // Ü (Funk öffnen/schließen)
+    KEY_RADIO_TALK: 20,     // Caps Lock
+    KEY_MEGAFON: 188,       // , (Megafon)
+    USE_HUD: true           // HUD aktivieren/deaktivieren
+};
